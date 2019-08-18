731c4d748c992d23ce2093b5076a7b4a4b79fced
Swapnil Ghodke <swapnilrghodke@gmail.com>
2019-08-18 23:28:56 +0530

    Added view model project

---
+++ ViewModels/StudentListViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using System.Collections.Generic;
+
+namespace ViewModels
+{
+    public class StudentListViewModel
+    {
+        private IList<Student> students;
+
+        public IList<Student> Students
+        {
+            get
+            {
+                return students;
+            }
+        }
+        
+        public StudentListViewModel(IDataAccess dataAccess)
+        {            
+            students = dataAccess.GetAllStudents();
+        }
+    }
+}
